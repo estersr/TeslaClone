849cad12001cb2a4d372798ee8909a35b310260b
estersr <estherramoos@gmail.com>
2022-06-09 19:40:16 -0300

    Tesla App

---
+++ components/StyledButton/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import {View, Text, Pressable} from 'react-native';
+import styles from './styles';
+
+
+
+
+
+const StyledButton = (props) => {
+
+    const {type, content, onPress} = props;
+
+
+    const backgroundColor = type === 'primary' ? '#171A20CC': '#FFFFFFA6';
+    const textColor = type === 'secondary' ? '#171A20': '#FFFFFF';
+
+
+
+return (
+    <View style= {styles.container}>
+        <Pressable 
+        style = {[styles.button, {backgroundColor: backgroundColor}]}
+            onPress = {() => onPress()}>
+            
+            <Text style = {[styles.text, {color: textColor}]}> {content} </Text>
+        </Pressable>
+    </View>
+
+
+);
+};
+
+export default StyledButton;
